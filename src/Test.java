108c72a9701ec518abc5125a703e59245641da87
realejandro <realejandrocf@gmail.com>
2022-05-12 19:52:24 -0500

    new realese

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,7 @@
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("");
+        System.out.println("This is batch12");
+    }
 }
